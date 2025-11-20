e98fd46ef9847bbb9f638a9e77c7e593cc074777
Charles Proust <konoha33@hotmail.fr>
2025-11-20 16:08:45 +0100

    edit search component

--- frontend/app/(tabs)/search.tsx
+++ frontend/app/(tabs)/search.tsx
@@ -1,21 +1,137 @@
+import EmptyState from "@/components/EmptyState";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import ProductCard from "@/components/ProductCard";
+import TextInput from "@/components/TextInput";
 import Wrapper from "@/components/Wrapper";
 import { AppColors } from "@/constants/theme";
-import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-
-const renderHeader = () => {
-	return (
-		<View>
-			<Text>Header</Text>
-		</View>
-	);
-};
+import { useProductStore } from "@/store/productStore";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { useEffect, useRef, useState } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const SearchScreen = () => {
-	const [searchQuery, setSearchQuery] = useState();
+	const [searchQuery, setSearchQuery] = useState("");
+	const searchTimeOutRef = useRef<number | null>(null);
+	const {
+		filteredProducts,
+		loading,
+		fetchProducts,
+		searchProductsRealTime, 
+		error
+	} = useProductStore();
 
+	useEffect(() => {
+		if (filteredProducts?.length === 0) {
+			fetchProducts();
+		}
+		return () => {
+			if (searchTimeOutRef.current) {
+				clearTimeout(searchTimeOutRef.current);
+			}
+		}
+	}, []);
 
-	return <Wrapper>{renderHeader()}</Wrapper>;
+	const handleSearchChange = (text: string) => {
+		setSearchQuery(text);
+		if (searchTimeOutRef.current) {
+			clearTimeout(searchTimeOutRef.current);
+		}
+		if (text.length >= 3) {
+			searchTimeOutRef.current = setTimeout(() => {
+				searchProductsRealTime(text)
+			}, 500);
+		} else {
+			searchProductsRealTime("");
+		}
+	};
+
+	const handleClearSearch = () => {
+		setSearchQuery("");
+		searchProductsRealTime("");
+	};
+
+	const renderHeader = () => {
+		return (
+			<View style={styles.header}>
+				<Text style={styles.title}>Search product</Text>
+				<View style={styles.searchRow}>
+					<View style={styles.searchContainer}>
+						<View style={styles.inputWrapper}>
+							<TextInput
+								value={searchQuery}
+								onChangeText={handleSearchChange}
+								placeholder="Search a product"
+								style={styles.searchInput}
+								inputStyle={styles.searchInputStyle}
+							/>
+							{searchQuery?.length > 0 && (
+								<TouchableOpacity
+									style={styles.clearButton}
+									onPress={handleClearSearch}
+								>
+									<AntDesign
+										name="close"
+										size={16}
+										color={AppColors.gray[500]}
+									/>
+								</TouchableOpacity>
+							)}
+						</View>
+						<TouchableOpacity
+							style={styles.searchButton}
+							onPress={() => searchProductsRealTime(searchQuery)}
+						>
+							<Ionicons
+								name="search"
+								size={24}
+								color={AppColors.background.primary}
+							/>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</View>
+		);
+	};
+
+	return (
+		<Wrapper>
+			{renderHeader()}
+			{loading ? (
+				<LoadingSpinner />
+			) : error ? (
+				<View>
+					<View style={styles.errorContainer}>
+						<Text style={styles.errorText}>{error}</Text>
+					</View>
+				</View>
+			) : filteredProducts?.length === 0 && searchQuery ? (
+				<EmptyState type="search" message="No matching product " />
+			) : (
+				<FlatList
+					data={searchQuery ? filteredProducts : []}
+					keyExtractor={(item) => item.id.toString()}
+					numColumns={2}
+					renderItem={({ item }) => (
+						<View style={styles.productContainer}>
+							<ProductCard product={item} customStyle={{ width: "100%" }} />
+						</View>
+					)}
+					contentContainerStyle={styles.productsGrid}
+					columnWrapperStyle={styles.columnWrapper}
+					ListFooterComponent={<View style={styles.footer} />}
+					ListEmptyComponent={
+						!searchQuery ? (
+							<View style={styles.emptyStateContainer}>
+								<Text style={styles.emptyStateText}>
+									Enter at least 3 letters to start the search
+								</Text>
+							</View>
+						) : null
+					}
+				/>
+			)}
+		</Wrapper>
+	);
 };
 
 export default SearchScreen;
