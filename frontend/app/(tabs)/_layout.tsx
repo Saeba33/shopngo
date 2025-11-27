f2bce7e76b738eae44e2ef1ed2e49361825f6265
Charles Proust <konoha33@hotmail.fr>
2025-11-27 09:11:16 +0100

    fix url config for stripe payment

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -50,6 +50,7 @@ export default function TabLayout() {
 			<Tabs.Screen name="signup" options={{ href: null }} />
 			<Tabs.Screen name="orders" options={{ href: null }} />
 			<Tabs.Screen name="payment" options={{ href: null }} />
+			<Tabs.Screen name="delivery_address" options={{ href: null }} />
 			<Tabs.Screen
 				name="product/[id]"
 				options={{ href: null, tabBarStyle: { display: "none" } }}
